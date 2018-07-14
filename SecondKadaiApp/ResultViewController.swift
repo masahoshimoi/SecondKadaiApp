2667a30ad5ea7e98c1ca69e433ae3eafe30efe69
masaho.shimoi <masaho.shimoi@techachademy.jp>
2018-07-14 22:24:15 +0900

    SecondKadaiAppの修正など

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by MacBook on 2018/07/14.
+//  Copyright © 2018年 Masaho Shimoi. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
+    
+    @IBOutlet weak var label: UILabel!
+    
+    // 受け取るためのプロパティ（変数）を宣言しておく
+    var names = "x"
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        // 上記では、x, y を 0 と宣言していたが、
+        // 1画面目のViewControllerから遷移するときにprepareForSegueで
+        // x, yの値を新たに代入されたので両方共 1 が入っている
+
+        label.text = "名前は \(names) です"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
+        
+        resultViewController.names = textField.text!
+        
+    }
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+    
+    
 }
 
